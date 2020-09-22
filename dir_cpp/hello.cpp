068e728be831d583f79525cf197672181aa43128
haska87 <haska87@gmail.com>
2020-09-22 16:38:46 +0900

    we add dir_cpp

---
+++ dir_cpp/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+void print_hello()
+{
+	cout << "Hello, this is CPP code!" << endl;
+	cout << "Hello Git!" << endl;
+}
---
+++ dir_cpp/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+int main()
+{
+	print_hello();
+	cout<< "The sum of 1 to 5 is " << sum(5) << endl;
+	return 0;
+}
